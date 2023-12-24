f88375341b25b75a5d3b1cac50028a76a83def96
friedebold <daniel@friedebold.net>
2023-12-24 01:24:32 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { io } from "socket.io-client";
 
-function App() {
+const socket = io("ws://localhost:8080");
+
+const App: React.FC = () => {
+  const [data, setData] = useState("");
+
+  socket.on("data", (text) => {
+    setData(text.data);
+    console.log(text);
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello World!</h1>
+      <p>{data}</p>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/serial.js
@@ -0,0 +1,35 @@
+// Create Server
+const http = require("http").createServer();
+
+const io = require("socket.io")(http, {
+  cors: { origin: "*" },
+});
+
+// Handshake
+io.on("connection", (socket) => {
+  console.log("user connected");
+});
+
+// Read Serial
+const { SerialPort, ReadlineParser } = require("serialport");
+
+const serialPort = new SerialPort({
+  path: "/dev/cu.usbserial-10",
+  baudRate: 9600,
+  dataBits: 8,
+  stopBits: 1,
+  parity: "none",
+  flowControl: false,
+});
+
+const parser = new ReadlineParser('\n\r');
+serialPort.pipe(parser);
+
+parser.on("data", function (data) {
+  console.log(data);
+  io.emit("data", { data: data });
+});
+
+http.listen(8080, () => {
+  console.log("listening on *:8080");
+});
